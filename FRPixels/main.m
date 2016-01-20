083aabe8b63a04dd35fe6300a4bbc1a5e99ecc02
Dylan Shine <dshine@fuzzproductions.com>
2016-01-20 09:48:18 -0500

    Initial Commit

---
+++ FRPixels/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FRPixels
+//
+//  Created by Dylan Shine on 1/20/16.
+//  Copyright © 2016 Fuzz Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
